557acc011064196a03b9994cd0a456328ad1633b
3P4R4 <3P4R4@users.noreply.github.com>
2021-10-10 06:32:22 +0000

    deploy: 12852f3b0145486360291edaa39eda5b305f3642

---
+++ _dist_/index.js
@@ -0,0 +1,43 @@
+import { registerImage } from "./lazy.js";
+
+/*Consumiendo API RandomFox  https://randomfox.ca/ */
+
+/*Creando una funcion RANDOM*/
+
+const minimo = 1;
+const maximo = 123;
+
+const azar = () => {
+    const random = Math.floor(Math.random() * (maximo - minimo)) + minimo;
+    return random;
+}
+
+const crearNodo = () => {
+    const container = document.createElement('div');
+    container.className = 'p4';
+
+    const imagen = document.createElement('img');
+    imagen.className = 'p-4';
+    imagen.classList.add('mx-auto');
+    imagen.setAttribute('width', '320');
+    imagen.dataset.src = `https://randomfox.ca/images/${azar()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+}
+
+
+
+
+// Agregando funciones al botón
+const boton = document.querySelector('button');
+const accion = () => {
+    const target = crearNodo();
+    agregar.append(target);
+    registerImage(target);
+
+}
+boton.addEventListener('click', accion);
+
+const agregar = document.getElementById('contenedor')
---
+++ _dist_/lazy.js
@@ -0,0 +1,38 @@
+const intersec = (entry) => {
+    return entry.isIntersecting;
+};
+
+const loadImage = (entry) => {
+    const container = entry.target;
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src;
+    imagen.src = url;
+
+    observer.unobserve(container);
+};
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.filter(intersec).forEach(loadImage)
+});
+
+export const registerImage = (imagen) => {
+    observer.observe(imagen);
+}
+
+
+
+//const observer = new IntersectionObserver(callback, option);
+
+/* const options = {
+    root: document.querySelector('.contenedor'),
+    rootMargin: '10px',
+    threshold: 0.5
+};
+
+const observer = new IntersectionObserver((img) => {
+    img.forEach((element) => console.log("element", element));
+}, options);
+
+export const target = document.querySelector("body");
+observer.observe(target); */
